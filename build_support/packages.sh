7568aaa01c8daece092098f1e51500e4e7c976e1
ycshenMac <shenchiri576@gmail.com>
2025-08-01 14:52:36 +0800

    macos llvm@14 used

--- build_support/packages.sh
+++ build_support/packages.sh
@@ -80,7 +80,7 @@ install_mac() {
   brew ls --versions coreutils || brew install coreutils
   brew ls --versions doxygen || brew install doxygen
   brew ls --versions git || brew install git
-  (brew ls --versions llvm | grep 12) || brew install llvm@12
+  # (brew ls --versions llvm | grep 12) || brew install llvm@12
 }
 
 install_linux() {
